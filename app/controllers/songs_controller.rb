82cbf46053a85272c5f0a71075d4e1cad3418636
Jonathan Luk <jluk87@gmail.com>
2019-04-04 01:53:10 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
   end
 
   def song_params
-    params.permit(:title, :artist_name, :released, :release_year, :genre)
+    params.require(:post).permit(:title, :artist_name, :released, :release_year, :genre)
   end
 
 
